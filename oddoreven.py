e913c83990f8192cf7abbf4cd5bd3cabd3d092d0
nirmalkumar12720 <nirmalkumar12720@gmail.com>
2020-08-06 18:21:48 +0530

    first commit

---
+++ oddoreven.py
@@ -0,0 +1,21 @@
+
+
+n=int(input("Enter How many members")
+print("ODD OR EVEN Game")
+a1="odd"
+a2="even"
+s1=int(input("enter the number"))
+s2=int(input("enter the number"))
+def iwin(i):
+    if i%2==0:
+        return even
+    return odd
+s11=iwin(s1)
+s22=iwin(s2)
+if s11==a1:
+    print("s1 is batting and s2 is bowling")
+else:
+    print("s2 is batting and s1 is bowling")
+
+
+
